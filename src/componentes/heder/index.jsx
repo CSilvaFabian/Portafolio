2f63d2f00d9338bf1bd89cb2ced91d78f09a3800
Fabian <cruzfabian292@mail.com>
2023-06-23 22:14:10 -0600

    se agregan todos los elementos del heder

--- src/componentes/heder/index.jsx
+++ src/componentes/heder/index.jsx
@@ -12,10 +12,20 @@ function Heder() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">Inicio
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link active" aria-current="page" href="#">Proyectos
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Habilidades
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Contacto
+          </a>
         </li>
       </ul>
     </div>
